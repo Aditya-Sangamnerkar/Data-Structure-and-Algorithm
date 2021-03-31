0ac00fa72fe2fe32f0c6d20fc042fac091c74617
Aditya Sangamnerkar <sangamnerkaram@rknec.edu>
2021-03-31 18:05:29 +0530

    .\minAbsDiff.java

---
+++ Arrays/src/minAbsDiff.java
@@ -0,0 +1,24 @@
+public class minAbsDiff {
+
+        public static int minDiff(int arr[], int n)
+        {
+            int minDiff = Math.abs(arr[n-1] - arr[0]);
+            for(int i=1;i<n;i++)
+            {
+                int currDiff = Math.abs(arr[i-1] - arr[i]);
+                if(currDiff < minDiff)
+                    minDiff = currDiff;
+            }
+            return minDiff;
+        }
+        public static void main(String[] args)
+        {
+            int arr[] = {8,-8,9,-9,10,-11,12};
+            int n = arr.length;
+            System.out.print("Array :");
+            for(int i=0;i<n;i++)
+                System.out.print(arr[i]+" ");
+            System.out.println("\nMinimum absolute adjcent difference : "+minDiff(arr,n));
+        }
+}
+
